73e6c834a4ecfb034cdff3cd25f2a13d8707bc5a
AhmedOsamaMohamed03 <121226325+AhmedOsamaMohamed03@users.noreply.github.com>
2024-05-16 17:16:38 +0300

    final

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -385,12 +385,13 @@ bool load(const char *file_name, void (**eip)(void), void **esp)
 
   /* Start address. */
   *eip = (void (*)(void))ehdr.e_entry;
-
+ file_deny_write(file);
+ thread_current()->executable = file;
   success = true;
 
 done:
   // TODO
-  file_deny_write(file);
+  
   /* We arrive here whether the load is successful or not. */
   // file_close(file);
   return success;
